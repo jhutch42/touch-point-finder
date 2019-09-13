dd6cb1c6c0ae8df90f1ec1e4d12bc5f0873dcd98
James <nmfuntouchable@gmail.com>
2019-09-13 11:40:09 -1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const CLUSTER_DISTANCE_MAX = 200;
 const TOUCHPOINT_SIZE = 20;
 const NORMAL = 0;
 const CREATE_CONSTELLATION = 1;
-const ANGLE_THRESHOLD = 10;
+const ANGLE_THRESHOLD = 20;
 
 let state = NORMAL;
 let point = null;
